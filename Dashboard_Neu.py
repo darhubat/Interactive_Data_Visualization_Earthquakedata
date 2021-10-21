8854a2b8096916f3cbc40bddcc8e2b9df83bc2b3
Hannibal-101 <77735740+Hannibal-101@users.noreply.github.com>
2021-10-21 12:20:48 +0200

    Update Dashboard_Neu.py

--- Dashboard_Neu.py
+++ Dashboard_Neu.py
@@ -21,9 +21,10 @@ df['number'] = 1 #add a new colum with the value 1 for each earthquake
 
 app.layout = html.Div(children=[
 
-    html.H1("Erdbeben Dashboard USA mit Dash and Plotly Express", style={'text-align': 'center'}),
+    html.H1("Erdbeben Dashboard USA mit Dash and Plotly Express"),
 
-    html.H2("Zahlen und Fakten zu den Erdbeben von 1965-2016", style={'text-align': 'center'}),
+    html.H2(children=["Zahlen und Fakten zu den Erdbeben von ", str(df['Jahr'].min()),  " - " , str(df['Jahr'].max()) ]),
+
 
     html.Div(id='header_div',
 
@@ -31,16 +32,16 @@ app.layout = html.Div(children=[
     html.Div([html.H3('Ereignis-Typ'),
                 dcc.Dropdown(id='type',
                  options=[
-                     {"label": "earthquake", "value": 'earthquake'}],
+                     {"label": "Erdbeben", "value": 'earthquake'}],
                  multi=False,
                  value='earthquake')]),
 
 
     html.Div([html.H3('Jahresfilter von/bis'),
               dcc.RangeSlider(id='Years',
-                         min=1965,
-                         max=2014,
-                         value=[2000, 2014],
+                         min=df['Jahr'].min(),
+                         max=df['Jahr'].max(),
+                         value=[df['Jahr'].min(), df['Jahr'].max()],
                          dots=True,
                          marks={1965: '1965',
                                 1975: '1975',
@@ -49,45 +50,40 @@ app.layout = html.Div(children=[
                                 2005: '2005',
                                 2015: '2015'},
                          allowCross=False,
-                         tooltip={'always_visible': True},
+                         tooltip={"placement": "bottom", 'always_visible': True},
                          disabled=False,
                          step=1, )]),
 
 
     html.Div([html.H3('Erdbebenstärke-Slider'),
-                dcc.Slider(id='mag-slider', min=5, max=8, step=0.1, value=df['mag'].max(),
-               tooltip={'always_visible': True},
+                dcc.RangeSlider(id='mag-slider', min=df['mag'].min(), max=df['mag'].max(), step=0.1, value=[df['mag'].min(), df['mag'].max()],
+               tooltip={"placement": "bottom", 'always_visible': True},
                marks={5.5: '5.5m', 6.0: '6.0m', 6.6: '6.5m', 7.0: '7.0m', 7.5: '7.5m', 8.0: '8.0m'}, )]),
 
     ]),
-    html.Div(children=[
-        dcc.Graph(id='earthquakeplot3', figure={}),
-        dcc.Graph(id='earthquakeplot4', figure={})],
-        style={'display': 'inline-block',
-               'vertical-align': 'top',
-               'margin-left': '3vw', 'margin-top': '3vw',
-               'width': '40vw', 'height': '40vh'}),
 
-    html.Div(children=[
-        dcc.Graph(id='earthquakeplot5', figure={}),
-        dcc.Graph(id='earthquakeplot6', figure={})],
-        style={'display': 'inline-block',
-               'vertical-align': 'top',
-               'margin-left': '3vw', 'margin-top': '3vw',
-               'width': '40vw', 'height': '40vh'}),
+    html.Div(id='content_div',children=[
+        html.Div(children=[
+            dcc.Graph(id='earthquakeplot1', figure={})]),
+        html.Div(children=[
+            dcc.Graph(id='earthquakeplot3', figure={})]),
+        html.Div(children=[
+            dcc.Graph(id='earthquakeplot2', figure={})]),
+        html.Div(children=[
+            dcc.Graph(id='earthquakeplot4', figure={})]),
+        ]),
 
-
-    #html.Div(id='footer',
-    #         children=[html.P('copyright simons und co')])
+    html.Div(id='footer',
+       children=[html.P('ADV HS21 Gruppe 4 | © D. Hubatka, S. Haug und F. Schnellmann')])
 ])
 
 
 
 @app.callback(
-    [Output(component_id='earthquakeplot3', component_property='figure'),
-     Output(component_id='earthquakeplot4', component_property='figure'),
-     Output(component_id='earthquakeplot5', component_property='figure'),
-     Output(component_id='earthquakeplot6', component_property='figure')],
+    [Output(component_id='earthquakeplot1', component_property='figure'),
+     Output(component_id='earthquakeplot3', component_property='figure'),
+     Output(component_id='earthquakeplot2', component_property='figure'),
+     Output(component_id='earthquakeplot4', component_property='figure')],
 
     [Input(component_id='type', component_property='value'),
      Input(component_id='mag-slider', component_property='value'),
@@ -98,72 +94,81 @@ app.layout = html.Div(children=[
 def update_graph(option_slctd, option_slctd2, years_slctd):
     dff = df.copy()
     dff = dff[dff["type"] == option_slctd]
-    dff = dff[dff["mag"] <= option_slctd2]
+    dff = dff[(dff["mag"] >= option_slctd2[0]) & (dff["mag"] <= option_slctd2[1])]
     dff = dff[(dff['Jahr'] >= years_slctd[0]) & (dff['Jahr'] <= years_slctd[1])]
 
 
     # Plotly Express
 
     # Earthquake-Map 1 (with year slider)
-    fig4 = px.scatter_geo(dff, lat='latitude', lon='longitude',
+    fig3 = px.scatter_geo(dff, lat='latitude', lon='longitude',
                           scope='usa', text='place',
                           hover_data=['Jahr'],
                           opacity=0.4, animation_frame='Jahr', projection='albers usa',
                           color='mag',
+                          color_continuous_scale="reds",
+                          # plasma, Bluered_r, aggrnyl, brwnyl, deep, thermal, orrd, redor, gray, temps, reds, ylorrd
                           size="mag",
                           labels={"mag": "Magnitude"})
-    fig4.update_layout(
-        title='Earthquakes in the United States (Timeline',
+    fig3.update_layout(
+        title='Erdbeben in den USA (mit Zeitschieberegler)',
         geo_scope='usa')
 
-
     # Earthquake-Map 2
-    fig3 = px.scatter_geo(dff, lat='latitude', lon='longitude',
+    fig1 = px.scatter_geo(dff, lat='latitude', lon='longitude',
                           scope='usa',
                           hover_data=['Jahr', 'place'],
                           opacity=0.4, projection='albers usa',
+                          color_continuous_scale="reds",
+                          # plasma, Bluered_r, aggrnyl, brwnyl, deep, thermal, orrd, redor, gray, temps, reds, ylorrd
                           color='mag',
                           size="mag",
                           labels={"mag": "Magnitude"})
 
-    fig3.update_layout(
-        title='Earthquakes in the United States',
-        geo_scope='usa')
+    fig1.update_layout(
+        title='Erdbeben in den USA (Karte)',
+        geo_scope='usa',
+    )
 
 
     #Barchart Earthquake per Year
-    fig5 = px.bar(dff, x="Jahr",
+    fig2 = px.bar(dff, x="Jahr",
              y="number",
              color='mag',
              hover_name="type",
              hover_data={'Jahr':True, 'mag':True, "place":True,'number':False},
-             title="Number of earthquakes per year",
-             color_continuous_scale='plasma', #plasma, Bluered_r, aggrnyl, brwnyl, deep
+             color_continuous_scale="reds", #plasma, Bluered_r, aggrnyl, brwnyl, deep, thermal, orrd, redor, gray, temps, reds, ylorrd
              labels={
-                     "number": "Number",
+                     "number": "Anzahl",
                      "mag": "Magnitude",
-                     "Jahr": "Year"},)
+                     "Jahr": "Jahr"},)
 
+    fig2.update_layout(
+        title="Anzahl der Erdbeben pro Jahr",
+
+    )
 
     # Table with detail for each Earthquake
-    fig6 = go.Figure(data=[go.Table(
+    fig4 = go.Figure(data=[go.Table(
         header=dict(
-            values=["Jahr", "Magnitude", "Depth (in Miles?)", "Place"],
-            font=dict(size=13),
-            line_color='silver',
+            values=["Jahr", "Magnitude", "Tiefe (in Miles)", "Ort"],
+            font=dict(size=15, family='Arial, bold'),
+            line_color='black',
             font_color='black',
-            fill_color='#e5ecf6',
+            fill_color='#BB2020',
             align="left"),
         cells=dict(
-            values=[dff["Jahr"].tolist(), dff["mag"].tolist(), dff["depth"].tolist(), dff["place"].tolist()],
-            line_color='silver',
+            values=[df["Jahr"].tolist(), df["mag"].tolist(), df["depth"].tolist(), df["place"].tolist()],
+            font=dict(size=14),
+            line_color='black',
             font_color='black',
-            fill_color='#e5ecf6',
+            fill_color='#f2c4c4',
             align="left"))
 
     ])
+    fig4.update_layout(title='Informationsboard')
 
-    return fig3, fig4, fig5, fig6
+    return fig1, fig2, fig3, fig4
 
 
 if __name__ == '__main__':
